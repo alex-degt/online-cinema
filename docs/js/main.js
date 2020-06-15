999e120dd94e47cb90ad062972e5c4d861c62176
Alexey <alex.xenkok@gmail.com>
2020-06-16 01:53:47 +0300

    v1.1

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-	$(".owl-carousel").owlCarousel({
+$(document).ready(function () {
+	$(function () {
+		$(".trends-card").slice(0, 5).show();
+		$("#load-more").on("click", function (e) {
+			e.preventDefault();
+			$(".trends-card:hidden").slice(0, 5).slideDown();
+		});
+	});
+
+	$(".slider-one").owlCarousel({
 		loop: true,
 		nav: true,
 		dots: false,
@@ -18,5 +26,30 @@ $(document).ready(function() {
 				items: 5,
 			},
 		},
+	});
+
+	$(".slider-two").owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			600: {
+				items: 5,
+			},
+			1000: {
+				items: 8,
+			},
+		},
+	});
+
+	$("#trends-card--wrapper").mixItUp();
+
+	$(".trends__filter").click(function () {
+		$(".trends__filter").not(this).removeClass("trends__filter--active");
+		$(this).toggleClass("trends__filter--active");
 	});
 });
